22a4716f81e75498599f5f91ab863f529e411c0c
Bhavyai Gupta <zbhavyai@gmail.com>
2021-06-04 20:44:38 +0530

    Completed the UI

--- src/StatsGUI.java
+++ src/StatsGUI.java
@@ -3,67 +3,82 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
- * 
- * Partial implementation of the Stats GUI
+ * Class to provide the front end UI for the Stats class
  *
- * @author
+ * @author Bhavyai Gupta
+ * @version 1.0
+ * @since June 4, 2021
  */
 public class StatsGUI extends JFrame implements ActionListener {
-	private JLabel prompt = new JLabel("Next Grade (press ENTER)");
-	private JTextField inField;
-	private JTextArea display;
-	private JButton reset, displayStats;
-	// TODO: Add stats instance variable
+    private JLabel prompt = new JLabel("Next Grade (press ENTER)");
+    private JTextField inField;
+    private JTextArea display;
+    private JButton reset, displayStats;
+    private Stats statistics;
 
-	public StatsGUI(String title) { 
-		
-		//TODO: Instantiate stats object
-		
-		inField = new JTextField(4);
-		inField.addActionListener(this);
-		
-		display = new JTextArea("", 5, 20);
-		
-		reset = new JButton("RESET");
-		reset.addActionListener(this);
-		
-		displayStats = new JButton("STATS");
-		displayStats.addActionListener(this);
+    public StatsGUI(String title) {
+        statistics = new Stats();
 
-		JPanel inputPanel = new JPanel();
-		inputPanel.setLayout(new GridLayout(0, 2));
-		inputPanel.add(prompt);
-		inputPanel.add(inField);
+        inField = new JTextField(4);
+        inField.addActionListener(this);
 
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(0, 2));
-		buttonPanel.add(displayStats);
-		buttonPanel.add(reset);
+        display = new JTextArea("", 5, 20);
 
-		Container contentPane = getContentPane();
-		contentPane.setLayout(new BorderLayout());
-		contentPane.add("North", inputPanel);
-		contentPane.add("Center", display);
-		contentPane.add("South", buttonPanel);
+        reset = new JButton("RESET");
+        reset.addActionListener(this);
 
-		setSize(500, 200);
-		setTitle(title);
-		setVisible(true);
-	}
+        displayStats = new JButton("STATS");
+        displayStats.addActionListener(this);
 
-	public void actionPerformed(ActionEvent e) {
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(0, 2));
+        inputPanel.add(prompt);
+        inputPanel.add(inField);
 
-		if (e.getSource() == inField) {
-			int number = Integer.parseInt(inField.getText());
-			// TODO: ADD THE GRADE TO stats
-		}
-		//TODO: Add handling of display button -  DISPLAY THE RESULTS
-		
-		// TODO: Add handling of reset button - CREATE A NEW stats
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(0, 2));
+        buttonPanel.add(displayStats);
+        buttonPanel.add(reset);
 
-	}
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.add("North", inputPanel);
+        contentPane.add("Center", display);
+        contentPane.add("South", buttonPanel);
 
-	public static void main(String[] args) {
-		new StatsGUI("Mean and median");
-	}
+        setSize(500, 200);
+        setTitle(title);
+        setVisible(true);
+    }
+
+    /**
+     * Catches the events by the ActionListener as a result of user interaction with
+     * the GUI interface
+     *
+     * @param e The action caught by the GUI
+     */
+    public void actionPerformed(ActionEvent e) {
+        // if user enters a new integer
+        if (e.getSource() == inField) {
+            int number = Integer.parseInt(inField.getText());
+            statistics.add(number);
+            inField.setText("");
+            display.append(String.format("%nAdded %d successfully", number));
+        }
+
+        // if user presses the STATS button
+        else if (e.getSource() == displayStats) {
+            display.setText(statistics.toString());
+        }
+
+        // if user presses the RESET button
+        else if (e.getSource() == reset) {
+            display.setText("");
+            this.statistics = new Stats();
+        }
+    }
+
+    public static void main(String[] args) {
+        new StatsGUI("Mean and median");
+    }
 }
